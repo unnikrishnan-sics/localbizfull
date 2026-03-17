7481b32b048e65a4c58fb18a48f426491e36f3e7
unnijsx <unniytman@gmail.com>
2026-03-17 10:31:38 +0530

    feat: add ChatBot component powered by Google Generative AI with a Material-UI interface. 50%

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -122,10 +122,10 @@ function App() {
           <Route path='/bussiness/addproduct' element={<BussinessAddProduct />} />
           <Route path='/bussiness/editproduct/:id' element={<BussinessEditProducts />} />
           <Route path='/bussiness/ViewProduct/:productId' element={<BusinessViewProduct />} />
-          <Route path='/bussiness/Community' element={<CommunityJoinForm />} />
+          {/* <Route path='/bussiness/Community' element={<CommunityJoinForm />} />
           <Route path='/bussiness/ViewEvents' element={<BusinessEvents />} />
           <Route path='/bussiness/ViewTrainning' element={<ViewTrainning />} />
-          <Route path='/bussiness/ViewWorkShops' element={<ViewWorkShop />} />
+          <Route path='/bussiness/ViewWorkShops' element={<ViewWorkShop />} /> */}
 
 
           {/* organiser */}
@@ -137,7 +137,7 @@ function App() {
           <Route path='/organiser/bussinessrequest' element={<OrganizerBussinessRequest />} />
           <Route path='/organiser/AboutUs' element={<OrganiserAboutUs />} />
           <Route path='/organiser/Contact' element={<OrganiserContact />} />
-          <Route path='/organiser/addevents' element={<OrganiserAddEvents />} />
+          {/* <Route path='/organiser/addevents' element={<OrganiserAddEvents />} />
           <Route path='/organiser/Viewevents' element={<OrganiserViewEvents />} />
           <Route path='/organiser/AddTrainning' element={<AddTrainning />} />
           <Route path='/organiser/ViewTrainning' element={<ViewTrainingsTable />} />
@@ -147,7 +147,7 @@ function App() {
           <Route path='/organiser/EditTrainning/:id' element={<EditTrainning />} />
           <Route path='/organiser/EditWorkShop/:id' element={<EditWorkShop />} />
           <Route path='/organiser/ViewProductList/:bussinessId' element={<OrganaiserViewProductList />} />
-          <Route path='/organiser/joined-members' element={<JointMembers />} />
+          <Route path='/organiser/joined-members' element={<JointMembers />} /> */}
 
 
         </Routes>
